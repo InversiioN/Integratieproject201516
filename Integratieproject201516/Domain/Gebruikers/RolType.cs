ee6d46c5a32a4efb84900a3d13ab75425252303c
brentboutmans <brent.boutmans@student.kdg.be>
2016-04-07 16:56:23 +0200

    g

---
+++ Integratieproject201516/Domain/Gebruikers/RolType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratieproject_Domain.Gebruikers
+{
+    public enum RolType
+    {
+        SuperAdmin=0,
+        Admin=1,
+        Moderator=2,
+        IngelogdeGebruiker=3
+    }
+}
---
+++ Integratieproject201516/Domain/Participatie/ProjectCategorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratieproject_Domain.Participatie
+{
+    public class ProjectCategorie
+    {
+        [Key]
+        public int projectCategorieID;
+        public int categorieNr;
+        public double minBedrag;
+        public double maxBedrag;
+    }
+}
