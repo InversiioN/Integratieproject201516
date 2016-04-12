0a40c34ac6df38a8c00c56f0ca179bb52df92f8a
Olivier De Smet <olivier.desmet@student.kdg.be>
2016-04-12 10:18:22 +0200

    1

--- Integratieproject201516/Domain/Begrotingen/Actie.cs
+++ Integratieproject201516/Domain/Begrotingen/Actie.cs
@@ -10,13 +10,10 @@ namespace Domain
         public int ActieID { get; set; }
         public Termijn TypeTermijn { get; set; }
         public string NaamBestuur { get; set; }
-        public string KorteBeschrijving { get; set; }
-        public string LangeBeschrijving { get; set; }
+        public string Beschrijving { get; set; }
         public DateTime FinancieelBoekjaar { get; set; }
         public double Uitgave { get; set; }
         public byte Afbeelding { get; set; }
         public Realisatie Realisatie { get; set; }
-
-
     }
 }
