6e54dcb6471144e3bffb10cd1ab657c8e4ea8024
Olivier De Smet <olivier.desmet@student.kdg.be>
2016-04-12 13:30:51 +0200

    Klasse UserStore

---
+++ Integratieproject201516/DAL/UserStore.cs
@@ -0,0 +1,13 @@
+﻿using Integratieproject_Domain.Gebruikers;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Integratieproject_DAL
+{
+    public class UserStore : UserStore<Gebruiker>
+    {
+        public UserStore() : base(new BPDbContext())
+        {
+
+        }
+    }
+}
