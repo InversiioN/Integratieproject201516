bb4e2b92e9b0d2804f335291d31d352d7f7a1503
Olivier De Smet <olivier.desmet@student.kdg.be>
2016-04-12 13:28:51 +0200

    Klasse MyUserManager

---
+++ Integratieproject201516/BL/MyUserManager.cs
@@ -0,0 +1,14 @@
+﻿using Integratieproject_DAL;
+using Integratieproject_Domain.Gebruikers;
+using Microsoft.AspNet.Identity;
+
+namespace Integratieproject_BL
+{
+    public class MyUserManager : UserManager<Gebruiker>
+    {
+        public MyUserManager() : base(new UserStore())
+        {
+
+        }
+    }
+}
